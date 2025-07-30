f190a36812c391329165360ea4d3811887c7ad64
Bhargava Shanmukha <124689250+Yuanlong999@users.noreply.github.com>
2025-07-30 11:44:17 +0530

    Update task.py

--- task.py
+++ task.py
@@ -1,4 +1,4 @@
 print("Welcome to The Band Name Generator!")
 city = input("Which city did you grow up in?\nAnswer: ")
 pet = input("What is the name of a pet of yours?\nAnswer: ")
-print("Your band name could be: " + city + " " + pe
+print("Your band name could be: " + city + " " + pet)
